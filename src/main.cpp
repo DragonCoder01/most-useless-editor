3be680491cab05f8f23393ad055c6e09d4b7a0f0
DragonCoder01 <dragon-coder@hotmail.com>
2020-05-13 21:21:47 +0200

    added editor window

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,25 @@ int main() {
 	raw();     // Also pass things like CTRL+C etc. to the program
 	keypad(stdscr, true); // Pass arrow keys, function keys etc. to the program
 
-	printw("Hello World!");
-	refresh();
+	auto* editor = newwin(LINES-2, COLS, 1, 0);
+	auto* top = newwin(1, COLS, 0, 0);
+
+	box(editor, 0, 0);
+	mvwprintw(editor, 0, 2, "|editor|");
+	wrefresh(editor);
 	
 	int code = -1;
 	while (code != 'q') {
 		code = getch();
 		if (code == '0' || code == '1') {
-			addch(code);
+			mvwaddch(editor, 1, 1, code);
+			move(2, 1);
+			x+=1;
 		}
+		
 		refresh();
+		wrefresh(editor);
+
 	}
 	
 	endwin();
