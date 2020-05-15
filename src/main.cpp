b4e01f36b7eef998af9ae7947acb4ded85b2e7cc
DragonCoder01 <dragon-coder@hotmail.com>
2020-05-15 12:20:05 +0200

    fixed some bugs and added printing :D

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,46 @@ int main() {
 	raw();     // Also pass things like CTRL+C etc. to the program
 	keypad(stdscr, true); // Pass arrow keys, function keys etc. to the program
 
-	auto* editor = newwin(LINES-2, COLS, 1, 0);
-	auto* top = newwin(1, COLS, 0, 0);
+	WINDOW* editor = newwin(LINES-2, COLS, 1, 0);
+//	auto* top = newwin(1, COLS, 0, 0);
+//	mvwprintw(editor, 0, 2, "|editor|");
+	wrefresh(editor);
+
+	printw("mu-edit");
+	refresh();
+
 
 	box(editor, 0, 0);
 	mvwprintw(editor, 0, 2, "|editor|");
 	wrefresh(editor);
 	
 	int code = -1;
+	int x = 0, y = 0;
+	int space = 0;
 	while (code != 'q') {
 		code = getch();
 		if (code == '0' || code == '1') {
-			mvwaddch(editor, 1, 1, code);
-			move(2, 1);
-			x+=1;
+			mvwaddch(editor, 1 + y, 1 + x, code);
+			++space;
+			if (space >= 8) {
+				space = 0;
+				waddch(editor, ' ');
+				++x;
+			}
+			++x;
+			if (x > COLS-3) {
+				x = 0;
+				++y;
+				if (y > LINES - 5) {
+					y = 0;
+				}
+			}
+			move(2 + y, 1 + x);
 		}
-		
-		refresh();
-		wrefresh(editor);
 
+		wrefresh(editor);
 	}
-	
+
 	endwin();
 
 	return 0;
